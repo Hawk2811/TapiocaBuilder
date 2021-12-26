84afa7acdfed353b8a20360313d3d6d3a213c163
Pedro <pedro@Pitty-Mac.local>
2021-12-26 12:50:13 -0300

    TapiocaBuilder 1.5 Portado para Windows

--- src/win32.cpp
+++ src/win32.cpp
@@ -1,13 +1,13 @@
 /*
 * TapiocaBuilder 1.0
-* Sistema de Compilação Via Scripts
+* Sistema de Compilaï¿½ï¿½o Via Scripts
 *
 * Para Usar o Programa
 * Crie um Arquivo Chamado "Buildscript.cmd"
-* e Para Usar Funcões de Limpeza Crie Outro Chamado "Cleanscript.cmd"
+* e Para Usar Funcï¿½es de Limpeza Crie Outro Chamado "Cleanscript.cmd"
 * o TapiocaBuilder Executa esses Scripts na Hora de Compilar ou Limpar o Projeto
 *
-* Dentro dos Scripts Você Coloca oque quiser exemplo
+* Dentro dos Scripts Vocï¿½ Coloca oque quiser exemplo
 *
 * para compliar C++ "clang++ arquivo.cpp"
 * executar arquivo python "python arquivo.py"
@@ -22,43 +22,44 @@
 
 
 using namespace std;
-// Configuração de Cores ANSI
+// Configuraï¿½ï¿½o de Cores ANSI
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define USE_ANSI false //Mude isto para false caso quiser usar em Windows 8.1/8/7/XP
 //Outras Cores ANSI
-//#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
 //#define ANSI_COLOR_BLUE    "\x1b[34m"
 //#define ANSI_COLOR_MAGENTA "\x1b[35m"
 //#define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
-//Funções de cor para Prompt do Windows para Windows 8.1/8/7/XP
+//Funï¿½ï¿½es de cor para Prompt do Windows para Windows 8.1/8/7/XP
 void resetcolors();
 void setcolor(WORD color);
 
-//Outras Configurações
-#define CLEAN_ENALED true //Habilitar a Função de Limpeza
+//Outras Configuraï¿½ï¿½es
+#define CLEAN_ENALED true //Habilitar a Funï¿½ï¿½o de Limpeza
 
 int main(int argc, char** argv) {
     if (argc < 2) { //Verifica se a Quantidade de Argumentos e Menor que 2
         printf("%s: Invalid Command. Try '%s help'", argv[0], argv[0]);
         return 0;
     }
-    else { //Verificação dos Argumentos
-        if (strcmp(argv[1], "help") == 0) { // Função de Ajuda
-            printf("TapiocaBuilder 1.0\n\n");
-            printf("Build - Build Project \n");
-            printf("Clean - Clean Project\n");
+    else { //Verificaï¿½ï¿½o dos Argumentos
+        if (strcmp(argv[1], "help") == 0) { // Funï¿½ï¿½o de Ajuda
+            printf("TapiocaBuilder 1.5\n\n");
+            printf("build - Build Project \n");
+            printf("clean - Clean Project\n");
+            printf("test - Execute Tests of Project\n");
             printf("\n");
         }
-        else if (strcmp(argv[1], "build") == 0) { //Função de Build
+        else if (strcmp(argv[1], "build") == 0) { //Funï¿½ï¿½o de Build
             clock_t start = clock();//Inicio da Contagem do Tempo
-            int exitcode = system("call BuildScript.cmd"); //Verifica o Codigo de Saida do Script de Compilação
+            int exitcode = system("call BuildScript.cmd"); //Verifica o Codigo de Saida do Script de Compilaï¿½ï¿½o
             if (exitcode == 0) {
                 clock_t stop = clock();//Final da Contagem do Tempo
-                double elapsed = (double)(stop - start) * 1000.0;//Calculando o Tempo de Execução
+                double elapsed = (double)(stop - start) * 1000.0;//Calculando o Tempo de Execuï¿½ï¿½o
                 if(USE_ANSI == true){
-                    printf(ANSI_COLOR_GREEN "Build Complete ! in %f Miliseconds " ANSI_COLOR_RESET, elapsed);//Mostra o Tempo de Execução em Milisegundos
+                    printf(ANSI_COLOR_GREEN "Build Complete ! in %f Miliseconds " ANSI_COLOR_RESET, elapsed);//Mostra o Tempo de Execuï¿½ï¿½o em Milisegundos
                 } else {
                     setcolor(2);
                     printf("Build Complete ! in %f Miliseconds ", elapsed);
@@ -66,7 +67,7 @@ int main(int argc, char** argv) {
                 }
                     
             }
-            else {//Uma Outra Função Caso o Script de Compilação Falhar
+            else {//Uma Outra Funï¿½ï¿½o Caso o Script de Compilaï¿½ï¿½o Falhar
                 clock_t stop = clock();
                 double elapsed = (double)(stop - start) * 1000.0;
                 if (USE_ANSI == true){
@@ -80,7 +81,7 @@ int main(int argc, char** argv) {
 
             }
         }
-        else if (strcmp(argv[1], "clean") == 0) {//Função de Limpar
+        else if (strcmp(argv[1], "clean") == 0) {//Funï¿½ï¿½o de Limpar
             if (CLEAN_ENALED == false){
                 if (USE_ANSI == true){
                     printf(ANSI_COLOR_RED "Clean Function Disabled !" ANSI_COLOR_RESET);    
@@ -97,7 +98,7 @@ int main(int argc, char** argv) {
             else {
                 clock_t start = clock();
                 int exitcode = system("call CleanScript.cmd"); //Verifica o Codigo de Saida do Script de Limpeza
-                if (exitcode == 0) {//Obs: O Codigo da Função de Limpar e bem parecido com o de Compilar
+                if (exitcode == 0) {//Obs: O Codigo da Funï¿½ï¿½o de Limpar e bem parecido com o de Compilar
                     clock_t stop = clock();
                     double elapsed = (double)(stop - start) * 1000.0;
                     if (USE_ANSI == true){
@@ -120,6 +121,38 @@ int main(int argc, char** argv) {
                     }
                     
 
+                }
+            }
+        } else if(strcmp(argv[1],"test") == 0){
+            if (USE_ANSI == true){
+                printf(ANSI_COLOR_YELLOW "Started Tests" ANSI_COLOR_RESET "\n");
+            } else {
+                setcolor(14);
+                printf("Started Tests\n");
+                resetcolors();
+            }
+            clock_t start = clock();
+            int exitcode = system("call TestScript.cmd");
+            if (exitcode == 0){
+                clock_t stop = clock();
+                double elapsed = (double)(stop - start) * 1000.0;
+                if (USE_ANSI == true) {
+                    printf(ANSI_COLOR_GREEN "Test Pass! in %f Miliseconds\n" ANSI_COLOR_RESET, elapsed);
+                } else {
+                    setcolor(2);
+                    printf("Test Pass! in %f Miliseconds\n", elapsed);
+                    resetcolors();
+                }
+                
+            } else {
+                clock_t stop = clock();
+                double elapsed = (double)(stop - start) * 1000.0;
+                if (USE_ANSI == true) {
+                    printf(ANSI_COLOR_RED "Test Failed ! in %f Miliseconds\n" ANSI_COLOR_RESET, elapsed);
+                } else {
+                    setcolor(4);
+                    printf("Test Failed ! in %f Miliseconds\n", elapsed);
+                    resetcolors();
                 }
             }
         }
